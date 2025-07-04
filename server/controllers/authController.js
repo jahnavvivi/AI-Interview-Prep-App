db9ccc80c023e8eb618fed49b98d3f6422a06d20
jahnavvivi <jahnavi.sharma1210@gmail.com>
2025-07-04 21:21:03 +0530

    changes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -87,6 +87,11 @@ const loginUser = async (req, res) => {
 //@access   Private(Requires JWT)
 const getUserProfile = async (req, res) => {
   try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
   } catch (error) {
     res
       .status(500)
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,11 +6,23 @@ const {
 
 const { protect } = require("../middlewares/authMiddleware");
 const express = require("express");
+const upload = require("../middlewares/uploadMiddleware");
 const router = express.Router();
 
 //auth routes
 router.post("/register", registerUser); //register a user
 router.post("/login", loginUser); //login a user
 router.get("/profile", protect, getUserProfile); //get a user profile
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+  res.status(200).json({ imageUrl });
+});
 
 module.exports = router;
